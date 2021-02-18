d3b6304721199a4be1ceab6abd82f387058e613d
Kauna Mohammed <kaunamohammed@gmail.com>
2021-02-18 12:31:15 +0100

    Fixed an issue causing this function to not sort elements properly
    the resulting array only contained the first element of the argument

--- algos/AlternatingSort.go
+++ algos/AlternatingSort.go
@@ -1,35 +1,38 @@
 package algos
 
-// AlternatingSort sorts an array of ints by placing the nums element at index 1 in 
-// then the element at index lens(nums) - 1 
-func AlternatingSort(nums []int) bool {
+
+// AlternatingSort sorts an array of ints by placing the nums element at index 1 in index 1 of a new array 
+// then the element at index lens(nums) - 1, then the element at index 2, then index lens(nums) - 2 and so on
+// until the array has been fully sorted.
+// if the resultong array of the sort is not in ascending order,
+// the function returns false along with an array of the indices of elements that first break the constraint
+// otherwise it returns true with the elements sorted in ascending order
+func AlternatingSort(nums []int) (bool, []int) {
 
 	if len(nums) == 0 || len(nums) == 1 {
-		return true
+		return true, nums
 	}
 
-	temp := make([]int, len(nums))
+	result := make([]int, len(nums))
 
 	leftPointer := 0
 	rightPointer := len(nums) - 1
 
-	temp[0] = nums[0]
+	result[0] = nums[0]
 
 	for i := 1; i < len(nums); i++ {
-		if leftPointer%2 == 0 {
-			temp[i] = nums[leftPointer]
+		if i % 2 == 0 {
+			result[i] = nums[leftPointer]
 		} else {
-			temp[i] = nums[rightPointer]
+			result[i] = nums[rightPointer]
 			rightPointer--
 			leftPointer++
 		}
-	}
 
-	for i := 0; i < len(temp)-1; i++ {
-		if temp[i] > temp[i+1] {
-			return false
+		if result[i] < result[i - 1] {
+			return false, []int { i, i - 1}
 		}
 	}
 
-	return true
+	return true, result
 }
