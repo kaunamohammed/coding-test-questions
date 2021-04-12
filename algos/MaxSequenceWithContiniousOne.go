6ff3590c03854e15088c6fc972faa4f55e2cfcf3
Kauna Mohammed <kaunamohammed@gmail.com>
2021-04-12 12:51:59 +0100

    finds the maximum number of ones when flipping at most k to one

---
+++ algos/MaxSequenceWithContiniousOne.go
@@ -0,0 +1,31 @@
+package algos
+
+import "github.com/kaunamohammed/godsa/utilities"
+
+// Given an array arr, this algorithm finds the maximum sequence of 1's that can be formed with at most k 0's
+func MaxSequenceWithContiniousOne(arr []int, k int) int {
+
+	zeroCounter := 0
+	leftPointer := 0
+	rightPointer := 0
+	maxTotal := 0
+
+	for rightPointer < len(arr) { 
+
+		if arr[rightPointer] == 0 {
+			zeroCounter++
+		}
+
+		for zeroCounter > k {
+			if arr[leftPointer] == 0 {
+				zeroCounter--
+			}			
+			leftPointer++	
+		}
+		
+		rightPointer++
+		maxTotal = utilities.Max(maxTotal, (rightPointer - leftPointer))
+	}
+
+	return maxTotal - zeroCounter
+}
